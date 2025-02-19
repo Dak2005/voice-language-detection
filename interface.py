8db89f86196aa2279985f3ff759bb77cbb18a261
dokku ayyappa <ayyappadokku89@gmail.com>
2025-02-19 14:53:20 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,14 +1,15 @@
+RENDER_BACKEND_URL = "https://your-app.onrender.com/detect_language/"
+
 import streamlit as st
 import requests
 
 st.title("Voice-Based Language Detection AI")
 
 if st.button("Speak & Detect"):
-    response = requests.get("http://0.0.0.0:8000")
+    response = requests.get(RENDER_BACKEND_URL)
     if response.status_code == 200:
         data = response.json()
         st.write(f"Detected Text: {data['text']}")
         st.write(f"Detected Language: {data['language']}")
     else:
         st.write("Error in processing")
-
